a6f38088f151781aeafee4e40f20320033bc1759
GriMary5566 <grimar_nsk@mail.ru>
2022-04-26 20:34:02 +0300

    Тест пуша

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -2,6 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+USER_ROLES = (
+    ('user', 'User'),
+    ('moderator', 'Moderator'),
+    ('admin', 'Admin')
+)
+
+
 class User(AbstractUser):
     bio = models.TextField(
         'Биография',
