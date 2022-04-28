6f4ab76ddc3eae19c7cdd787717f7427e1f6b007
Дмитрий Филимонов <atrom@list.ru>
2022-04-28 20:51:30 +0300

    UPD: custom management command

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,124 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+REVIEW_SCORE = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+    )
+    description = models.TextField()
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+    )
+
+
+class GenreTitle(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+    )
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        blank=True,
+        null=True,
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        blank=True,
+        null=True,
+    )
+    score = models.IntegerField(
+        default=1,
+        choices=REVIEW_SCORE,
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author',
+            ),
+        ]
+
+    def __str__(self):
+        return (
+            f'Отзыв {self.author.first_name} {self.author.last_name}'
+            f'на произведение {self.title.name}.'
+        )
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        blank=True,
+        null=True,
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        blank=True,
+        null=True,
+    )
+    pub_date = models.DateTimeField('Дата добавления', auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f'Комментарий {self.author.first_name} {self.author.last_name}'
+            f'от {self.pub_date}.'
+        )
